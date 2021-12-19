b59151dcf21a2dda95cdf76dd5dcd700c59752e0
while-goo <1263637745@qq.com>
2021-12-19 17:14:08 +0800

    毕设后端初始化

---
+++ parent/admin/src/main/java/white/goo/controller/UserController.java
@@ -0,0 +1,22 @@
+package white.goo.controller;
+
+import org.springframework.web.bind.annotation.*;
+import white.goo.dto.R;
+import white.goo.entity.User;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin
+public class UserController {
+
+    @PostMapping("/login")
+    public R login(@RequestBody User user){
+        return R.ok().data("token","admin");
+    }
+
+    @GetMapping("/info")
+    public R info(){
+        return R.ok().data("roles", "[admin]").data("name","admin").data("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+    }
+
+}
